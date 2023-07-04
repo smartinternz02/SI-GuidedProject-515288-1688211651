e82c60a9696979d9e56c5a9bcdd403d4ee637475
Raksha Gupta <125679621+raksha2822@users.noreply.github.com>
2023-07-04 18:13:59 +0530

    Add files via upload

---
+++ project/AIExternship/Scripts/breadability-3.10-script.py
@@ -0,0 +1,33 @@
+#!"D:\VIT Stuff\AI-Internship\AIExternship\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'breadability==0.1.20','console_scripts','breadability-3.10'
+import re
+import sys
+
+# for compatibility with easy_install; see #2198
+__requires__ = 'breadability==0.1.20'
+
+try:
+    from importlib.metadata import distribution
+except ImportError:
+    try:
+        from importlib_metadata import distribution
+    except ImportError:
+        from pkg_resources import load_entry_point
+
+
+def importlib_load_entry_point(spec, group, name):
+    dist_name, _, _ = spec.partition('==')
+    matches = (
+        entry_point
+        for entry_point in distribution(dist_name).entry_points
+        if entry_point.group == group and entry_point.name == name
+    )
+    return next(matches).load()
+
+
+globals().setdefault('load_entry_point', importlib_load_entry_point)
+
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(load_entry_point('breadability==0.1.20', 'console_scripts', 'breadability-3.10')())
